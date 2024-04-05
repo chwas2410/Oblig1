9926aa92ae0494bcd90d41aab2fa73dd61750c8f
chwas2410 <chwas2410@oslomet.no>
2024-04-05 13:30:29 +0200

    Rett

--- src/main/java/com/example/idk/IdkApplication.java
+++ src/main/java/com/example/idk/IdkApplication.java
@@ -1,13 +1,13 @@
-package com.example.oblig1;
+package com.example.idk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Oblig1Application {
+public class IdkApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Oblig1Application.class, args);
+        SpringApplication.run(IdkApplication.class, args);
     }
 
 }
